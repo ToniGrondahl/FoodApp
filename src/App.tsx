0b4149661907549f0ef6a0f8157fba1ceddf332a
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-12-05 12:40:21 +0200

    Routet

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Login from './pages/Login';
+import Register from  './pages/Register'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,6 +43,12 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
+        <Route exact path="/Login">
+            <Login />
+          </Route>
+          <Route exact path="/Register">
+            <Register />
+          </Route>
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
