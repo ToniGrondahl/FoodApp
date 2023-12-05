2f65ffb04ac85e38b068358c6648edc53cd4df6f
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-12-05 13:16:24 +0200

    Loginia

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonButton  } from '@ionic/react';
 import './Login.css';
 
 const Login: React.FC = () => {
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Tab 3</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          
+    <IonPage>
+        <IonContent >
+            <div className="card-center">
+                <IonCard>
+                <IonCardHeader>
+                    <IonCardTitle>Food App</IonCardTitle>
+                </IonCardHeader>
+                    <IonCardContent>
+                        <IonItem>
+                            <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****"></IonInput>
+                        </IonItem>
+                        <IonButton expand="block">Log In</IonButton>
+                        <IonCardSubtitle>Don't have an Account</IonCardSubtitle>
+                        <IonButton fill="outline" expand="block" >Sign Up</IonButton>
+                    </IonCardContent>
+                    </IonCard>
+            </div>
         </IonContent>
-      </IonPage>
+    </IonPage>
     );
   };
   
