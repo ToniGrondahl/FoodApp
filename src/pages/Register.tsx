b8c0990053b7ce82a2b4b5625a6f645f6e9f0f01
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-12-05 13:57:30 +0200

    Login ja register layout

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,23 +1,33 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonCardContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonButton  } from '@ionic/react';
 import './Register.css';
 
 const Register: React.FC = () => {
     return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Tab 3</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          
+        <IonContent >
+            <div className="flex-center">
+                <IonCard>
+                <IonCardHeader>
+                    <IonCardTitle>Create account</IonCardTitle>
+                </IonCardHeader>
+                    <IonCardContent>
+                        <IonItem>
+                            <IonInput label="Username" type="text" labelPlacement="stacked" placeholder="example@gmail.com"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****"></IonInput>
+                        </IonItem>
+                        <IonButton id='button' expand="block">Sign Up</IonButton>
+                        
+                    </IonCardContent>
+                    </IonCard>
+            </div>
         </IonContent>
-      </IonPage>
+    </IonPage>
     );
   };
   
