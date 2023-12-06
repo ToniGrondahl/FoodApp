50af2c8cf5b241e800e19f9fe44e1f7dd8c52a5c
MikkoMal1 <Mikko.Malinen@student.laurea.fi>
2023-12-06 19:01:58 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { star } from 'ionicons/icons';
+import { IonContent, IonHeader, IonCardContent, IonPage, IonTitle, IonToolbar, IonTextarea, IonText, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -11,12 +13,61 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="home page" />
+
+        <IonToolbar>
+          <IonTitle>Recomended Restaurants</IonTitle>
+        </IonToolbar>
+
+        <IonCard>
+          <img src='images/kalasipsi.jpeg' alt='fishChips' />
+          <IonCardHeader>
+            <IonCardTitle>Authentic Fish&Chips</IonCardTitle>
+            <IonCardSubtitle color="tertiary">Feels like you are in UK</IonCardSubtitle>
+            <IonIcon icon={star} color="gold"> </IonIcon>
+            <IonIcon icon={star} color="gold"> </IonIcon>
+            <IonIcon icon={star} color="gold"> </IonIcon>
+
+
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <img src='images/keittoa.jpeg' alt='keittoa' />
+          <IonCardHeader>
+            <IonCardTitle>Jouni's soup corner</IonCardTitle>
+            <IonCardSubtitle color="tertiary">Mouth watering soups since 1998</IonCardSubtitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <img src='images/pizzza.jpeg' alt='pizza' />
+          <IonCardHeader>
+            <IonCardTitle>Antalanya pizzeria</IonCardTitle>
+            <IonCardSubtitle color="tertiary">Best pizza in Finland</IonCardSubtitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <img src='images/sushi.jpg' alt='sushi' />
+          <IonCardHeader>
+            <IonCardTitle>King sushi</IonCardTitle>
+            <IonCardSubtitle color="tertiary">Fresh everyday</IonCardSubtitle>
+          </IonCardHeader>
+        </IonCard>
+
+        <IonCard>
+          <img src='images/kahvila.jpg' alt='kahvila' />
+          <IonCardHeader>
+            <IonCardTitle>Cafe Ursus</IonCardTitle>
+            <IonCardSubtitle color="tertiary">Hot coffee and fresh pasteries</IonCardSubtitle>
+          </IonCardHeader>
+        </IonCard>
+
+
+
+
+
+
       </IonContent>
     </IonPage>
   );
