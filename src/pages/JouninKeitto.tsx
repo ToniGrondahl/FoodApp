95a6b9f6cbee724e0759ff7ba1eeb092bb2cfb90
MikkoMal1 <Mikko.Malinen@student.laurea.fi>
2023-12-08 19:03:29 +0200

    Create JouninKeitto.tsx

---
+++ src/pages/JouninKeitto.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { star, starHalf, starOutline } from 'ionicons/icons';
+import { IonContent, IonHeader, IonThumbnail, IonCardContent, IonPage, IonTitle, IonToolbar, IonTextarea, IonText, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Menu.css';
+
+const JouninKeitto: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar color="tertiary">
+                    <IonTitle className='Menu'>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+
+                <IonCard>
+                    <img src='images/keittoa.jpeg' alt='keittoa' />
+                    <IonCardHeader color="tertiary">
+                        <IonCardTitle>Jouni's soup corner</IonCardTitle>
+                    </IonCardHeader>
+                </IonCard>
+
+
+
+
+
+            </IonContent>
+        </IonPage>
+
+    );
+};
+
+export default JouninKeitto;
