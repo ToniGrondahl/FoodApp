6b2145932fc3630d4d9b81803c7b43e08c38834c
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2023-12-08 13:11:13 +0200

    ei toimi vielä

--- src/pages/login&register/Login.tsx
+++ src/pages/login&register/Login.tsx
@@ -1,8 +1,17 @@
-import { IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  } from '@ionic/react';
 import {IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonButton  } from '@ionic/react';
 import './Login.css';
+import { useState } from 'react';
 
 const Login: React.FC = () => {
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function LogInUser() {
+        console.log(email, password);
+    }
+
     return (
     <IonPage>
         <IonContent >
@@ -13,14 +22,14 @@ const Login: React.FC = () => {
                 </IonCardHeader>
                     <IonCardContent>
                         <IonItem>
-                            <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com"></IonInput>
+                            <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com" onIonChange={(e: any) => setEmail(e.target.value)} required></IonInput>
                         </IonItem>
                         <IonItem>
-                            <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****"></IonInput>
+                            <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****" onIonChange={(e: any) => setPassword(e.target.value)} required></IonInput>
                         </IonItem>
-                        <IonButton id='button' expand="block">Log In</IonButton>
+                        <IonButton id='button' expand="block" type='submit' onClick={() => logInWithEmailAndPassword(email, password)}>Log In</IonButton>
                         <p>Don't have an Account</p>
-                        <IonButton id='button' fill="outline" expand="block" >Sign Up</IonButton>
+                        <IonButton id='button' fill="outline" expand="block" routerLink='./register'>Sign Up</IonButton>
                     </IonCardContent>
                     </IonCard>
             </div>
@@ -30,4 +39,4 @@ const Login: React.FC = () => {
   };
   
   export default Login;
-  
+
