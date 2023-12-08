4edd3e0bc43a87769f85109c7a9bf205d677db44
SamiHeina <samiheinanen96@gmail.com>
2023-12-08 18:09:12 +0200

    restaurants

--- src/pages/Restaurants.tsx
+++ src/pages/Restaurants.tsx
@@ -1,8 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Restaurants.css';
+import React from 'react';
+import { star } from 'ionicons/icons';
+import {
+  IonContent,IonHeader,IonPage,IonTitle,IonToolbar,IonCard,IonCardHeader,
+  IonCardTitle,IonCardSubtitle,IonIcon,IonGrid,IonRow, IonCol,IonButton} 
+from '@ionic/react';
+import './restaurants.css';
 
-const Restaurants: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -11,15 +15,87 @@ const Restaurants: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Restaurants</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="restaurants page" />
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonCard className="cardsize">
+              <img src='images/kalasipsi.jpeg' alt='fishChips' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>Authentic Fish&Chips</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Feels like you are in UK</IonCardSubtitle>
+         </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+            <IonCol>
+            <IonCard className="cardsize">
+              <img src='images/keittoa.jpeg' alt='keittoa' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>Jouni's soup corner</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Mouth watering soups since 1998</IonCardSubtitle>
+          </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonCard className="cardsize">
+              <img src='images/pizzza.jpeg' alt='pizza' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>Antalanya pizzeria</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Best pizza in Finland</IonCardSubtitle>
+          </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+            <IonCol>
+            <IonCard className="cardsize">
+              <img src='images/sushi.jpg' alt='sushi' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>King sushi</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Fresh everyday</IonCardSubtitle>
+          </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonCard className="cardsize">
+            <img src='images/kahvila.jpg' alt='kahvila' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>Cafe Ursus</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Hot coffee and fresh pasteries</IonCardSubtitle>
+          </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+            <IonCol>
+            <IonCard className="cardsize">
+              <img src='images/Tartarillas.jpeg' alt='Tartarilla' className='images' />
+          <IonCardHeader>
+            <IonCardTitle className='header'>Tartarilla's</IonCardTitle>
+            <IonCardSubtitle color="tertiary" className='desc'>Spicy Mexican Food</IonCardSubtitle>
+          </IonCardHeader>
+            <IonButton expand="block" color="success" size="small" className='buttoninfo'>Info</IonButton>
+            <IonButton expand="block" color="tertiary" size="default" className='buttonmenu'>Menu</IonButton>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+          
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Restaurants;
+export default Home;
+
+
+
