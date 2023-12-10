1ba04e6d4b50e00bbdcdb5ba3c69b66fc1249528
MikkoMal1 <Mikko.Malinen@student.laurea.fi>
2023-12-10 21:42:42 +0200

    Create Jouninkeitto.tsx

---
+++ src/pages/Menu/Jouninkeitto.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { star, starHalf, starOutline, cartOutline } from 'ionicons/icons';
+import { IonContent, IonHeader, IonThumbnail, IonCardContent, IonPage, IonButton, IonTitle, IonToolbar, IonTextarea, IonText, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonIcon, IonList, IonLabel } from '@ionic/react';
+import './Menu.css';
+
+const Home: React.FC = () => {
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+
+                <IonHeader>
+                    <img className='kuva' alt="keittoa" src='images/keittoa.jpeg' />
+                    <IonToolbar color="tertiary">
+                        <IonTitle className='Menu'>Jouni's soup corner</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+
+                <IonCard>
+                    <IonCardContent>
+
+                        <IonItem>
+                            <IonLabel>1. Salmon soup</IonLabel>
+                            <IonLabel> 13,99€</IonLabel>
+                            <IonButton>
+                                <IonIcon slot="icon-only" icon={cartOutline}></IonIcon>
+                            </IonButton>
+                        </IonItem>
+
+                    </IonCardContent>
+                </IonCard>
+
+                <IonCard>
+                    <IonCardContent>
+
+                        <IonItem>
+                            <IonLabel>2. Chicken soup</IonLabel>
+                            <IonLabel> 9,99€</IonLabel>
+                            <IonButton>
+                                <IonIcon slot="icon-only" icon={cartOutline}></IonIcon>
+                            </IonButton>
+                        </IonItem>
+
+                    </IonCardContent>
+                </IonCard>
+
+
+                <IonCard>
+                    <IonCardContent>
+
+                        <IonItem>
+                            <IonLabel>3. Tomato soup</IonLabel>
+                            <IonLabel> 7,99€</IonLabel>
+                            <IonButton>
+                                <IonIcon slot="icon-only" icon={cartOutline}></IonIcon>
+                            </IonButton>
+                        </IonItem>
+
+                    </IonCardContent>
+                </IonCard>
+
+                <IonCard>
+                    <IonCardContent>
+
+                        <IonItem>
+                            <IonLabel>4. French onion soup</IonLabel>
+                            <IonLabel> 10,99€</IonLabel>
+                            <IonButton>
+                                <IonIcon slot="icon-only" icon={cartOutline}></IonIcon>
+                            </IonButton>
+                        </IonItem>
+
+                    </IonCardContent>
+                </IonCard>
+
+                <IonCard>
+                    <IonCardContent>
+
+                        <IonItem>
+                            <IonLabel>5. Meat soup</IonLabel>
+                            <IonLabel> 11,99€</IonLabel>
+                            <IonButton>
+                                <IonIcon slot="icon-only" icon={cartOutline}></IonIcon>
+                            </IonButton>
+                        </IonItem>
+
+                    </IonCardContent>
+                </IonCard>
+
+            </IonContent >
+        </IonPage >
+    );
+};
+
+export default Home;
