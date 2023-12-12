92e45ab120f05da43c26c8683783a329c04ffbf8
ToniGrondahl <toni.grondahl@outlook.com>
2023-12-12 23:36:51 +0200

    taustaväriä

--- src/pages/login&register/Register.tsx
+++ src/pages/login&register/Register.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import {IonCardContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonButton, useIonToast  } from '@ionic/react';
+import { IonCardContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonButton, useIonToast } from '@ionic/react';
 import './Register.css';
 import { useState } from 'react';
 import { registerUserValidation } from './firebase'
@@ -14,60 +14,60 @@ const Register: React.FC = () => {
     const [present] = useIonToast();
 
     const presentToast = (message: string) => {
-    present({
-      message: message,
-      duration: 5000,
-      position:'bottom',
-    });
-  };
+        present({
+            message: message,
+            duration: 5000,
+            position: 'bottom',
+        });
+    };
 
     async function registerUser() {
-        if (password !== Cpassword){
+        if (password !== Cpassword) {
             presentToast('Passwords do not match')
-            
-    } if (email.trim() === '' || password.trim() === '') {
-            presentToast('Email and password are required')
-           
-    }
 
-    const res = await registerUserValidation(email, password)
-    if (res === true) {
-        presentToast('account created')
-        setShowError(true);
-    } else {
-        presentToast('creating account failed')
-       
+        } if (email.trim() === '' || password.trim() === '') {
+            presentToast('Email and password are required')
+
+        }
+
+        const res = await registerUserValidation(email, password)
+        if (res === true) {
+            presentToast('account created')
+            setShowError(true);
+        } else {
+            presentToast('creating account failed')
+
+        }
     }
-}
 
     return (
-      <IonPage>
-        <IonContent >
-            <div className="flex-center">
-                <IonCard>
-                <IonCardHeader>
-                    <IonCardTitle>Create account</IonCardTitle>
-                </IonCardHeader>
-                    <IonCardContent>
-                        
-                        <IonItem>
-                            <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com" onIonChange={(e: any) => setEmail(e.target.value)}></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****" onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
-                        </IonItem>
-                        <IonItem>
-                            <IonInput label="Password again" type="password" labelPlacement="stacked" placeholder="*****" onIonChange={(e: any) => setCPassword(e.target.value)}></IonInput>
-                        </IonItem>
-                        <IonButton id='button' expand="block" type='submit' onClick={registerUser}>Sign Up</IonButton>
-                        {showError ? <Redirect to="/home" /> : null}
-                        <p>Already have an account?  <Link to="./login"> Login</Link></p>
-                    </IonCardContent>
+        <IonPage>
+            <IonContent color={'tertiary'}>
+                <div className="flex-center">
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonCardTitle>Create account</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+
+                            <IonItem>
+                                <IonInput label="Email" type="email" labelPlacement="stacked" placeholder="example@gmail.com" onIonChange={(e: any) => setEmail(e.target.value)}></IonInput>
+                            </IonItem>
+                            <IonItem>
+                                <IonInput label="Password" type="password" labelPlacement="stacked" placeholder="*****" onIonChange={(e: any) => setPassword(e.target.value)}></IonInput>
+                            </IonItem>
+                            <IonItem>
+                                <IonInput label="Password again" type="password" labelPlacement="stacked" placeholder="*****" onIonChange={(e: any) => setCPassword(e.target.value)}></IonInput>
+                            </IonItem>
+                            <IonButton id='button' expand="block" type='submit' onClick={registerUser}>Sign Up</IonButton>
+                            {showError ? <Redirect to="/home" /> : null}
+                            <p>Already have an account?  <Link to="./login"> Login</Link></p>
+                        </IonCardContent>
                     </IonCard>
-            </div>
-        </IonContent>
-    </IonPage>
+                </div>
+            </IonContent>
+        </IonPage>
     );
-  };
-  
-  export default Register;
+};
+
+export default Register;
