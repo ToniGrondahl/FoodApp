770f3532168095e5d524ce70f2ccd3a89a3f2a60
ToniGrondahl <toni.grondahl@outlook.com>
2023-12-12 16:23:25 +0200

    Map API works

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Search.css';
+import React, { useRef, useEffect } from 'react';
+import {
+  IonCol,
+  IonRow,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonViewWillEnter,
+} from '@ionic/react';
+import { GoogleMap } from '@capacitor/google-maps';
 
 const Search: React.FC = () => {
+  const mapRef = useRef<HTMLElement>();
+  let newMap: GoogleMap;
+
+  useEffect(() => {
+    setTimeout(() => {
+      async function createMap() {
+        if (!mapRef.current) return;
+
+        newMap = await GoogleMap.create({
+          id: 'my-cool-map',
+          element: mapRef.current,
+          apiKey: 'AIzaSyAbQz_bURj7AJOTMy2Izq8nXjSq599L7uU',
+          config: {
+            center: {
+              lat: 60.293508960349065,
+              lng: 25.038473304410385,
+            },
+            zoom: 12,
+          },
+        });
+      }
+
+      createMap();
+    }, 1000);
+  }, []);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar color="tertiary">
-          <IonTitle className='search'>Search</IonTitle>
+      <IonHeader >
+        <IonToolbar className="toolbar" color="tertiary">
+          <IonTitle className="account" >Search</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +51,18 @@ const Search: React.FC = () => {
             <IonTitle size="large">Search</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="search page" />
+        <IonRow>
+          <IonCol size="12">
+            <capacitor-google-map
+              ref={mapRef}
+              style={{ display: 'inline-block', width: '100%', height: '100vh' }}
+            ></capacitor-google-map>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol size="12">
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
